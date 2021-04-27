517fdbf41a0cfbaa594a0ec3f45772b5c6d00f74
Tsion Rosen <tsionrosen@hotmail.com>
2021-04-28 02:00:24 +0300

    modification de formik a un form normal

--- src/Components/LeadsForm.js
+++ src/Components/LeadsForm.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import utils from '../utils'
 import '../Components_Css/leadsForm.css'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
         root: {
@@ -14,84 +14,82 @@ const useStyles = makeStyles((theme) => ({
         },
 }));
 
-    
 
 const LeadsForm = (props) => {
 
+    const [prenom, setPrenom] = useState("")
+    const [nom, setNom] = useState("")
+    const [numeroDeTelephone, setNumeroDeTelephone] = useState(0)
+    const [email, setEmail] = useState("")
+    const [formation, setFormation] = useState("")
     const classes = useStyles();
+
+    const errors = {}
+
+    if (!prenom) {
+        errors.prenom = "Obligatoire";
+    } else if(prenom.length<3){
+        errors.prenom = 'veuillez saisir votre Prenom'
+    }
+
+    if (!nom) {
+        errors.nom = 'Obligatoire';
+    } else if(nom.length<3){
+        errors.nom = 'veuillez saisir votre Nom de Famille'
+    }
+
+    if (!numeroDeTelephone) {
+        errors.numeroDeTelephone = 'Obligatoire';
+        } else if(numeroDeTelephone.length<10){
+            errors.numeroDeTelephone = 'votre numero de telephone doit contenir 10 numeros'
+        }
+
+        if (!email) {
+            errors.email = 'Obligatoire';
+        }
+
+        if (!formation) {
+            errors.formation = 'Obligatoire';
+        }
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+
+        let obj = {
+            prenom,
+            nom, 
+            numeroDeTelephone,
+            email,
+            formation
+        }
+
+        let resp = await utils.addNewLead("http://localhost:8001/formation-cpf/", obj)
+                    alert("Votre inscription a bien etait envoyer!")
+    }
 
     return (
     <div >
         <h2 style={{textAlign:"center", marginTop:"30px"}}> Saisir votre droit a la formation </h2>
         <div className="formStructure">
-        <Formik initialValues={{ prenom: '', nom: '', numeroDeTelephone: '', email: '', formation: '' }}
-            validate={values => {
-                const errors = {};
+        
+        <form onSubmit={handleSubmit} className="form" className={classes.root} noValidate autoComplete="off">
 
-                if (!values.prenom) {
-                errors.prenom = 'Obligatoire';
-                } else if(values.prenom.length<3){
-                    errors.prenom = 'veuillez saisir votre Prenom'
-                }
+            <TextField id="standard-basic" className="input" onChange={e=> setPrenom(e.target.value)} label="Prenom" required/>
+                {/* <div> {errors.prenom} </div> */}
+            <TextField id="standard-basic" className="input" onChange={e=> setNom(e.target.value)} label="Nom"/> <br/><br/>
 
-                if (!values.nom) {
-                    errors.nom = 'Obligatoire';
-                } else if(values.nom.length<3){
-                    errors.nom = 'veuillez saisir votre Nom de Famille'
-                }
+            <TextField id="standard-basic" className="input" onChange={e=> setNumeroDeTelephone(e.target.value)} label="Numero de Telephone"/>
 
-                if (!values.numeroDeTelephone) {
-                    errors.numeroDeTelephone = 'Obligatoire';
-                    } else if(values.numeroDeTelephone.length<10){
-                        errors.numeroDeTelephone = 'votre numero de telephone doit contenir 10 numeros'
-                    }
+            <TextField id="standard-basic" className="input" onChange={e=> setEmail(e.target.value)} type="email" label="E-mail"/><br/><br/>
 
-                    if (!values.email) {
-                        errors.email = 'Obligatoire';
-                    }
+            <TextField id="standard-basic" className="input" onChange={e=> setFormation(e.target.value)} label="Choix de Formation"/> <br/><br/> <br/>
 
-                    if (!values.formation) {
-                        errors.formation = 'Obligatoire';
-                    }
+            <Button style={{marginLeft:"23%"}} type="submit" variant="contained" color="primary">
+                Register
+            </Button>
+            {/* <input type="submit" style={{marginLeft:"23%"}} value="Register" className="submitButton" /><br/> */}
 
-                return errors;
-            }}
-
-            onSubmit={async (values, { setSubmitting }) => {
-
-                    let resp = await utils.addNewLead("http://localhost:8001/formation-cpf/", values)
-                    alert("Votre inscription a bien etait envoyer!")
-            }}
-        >
-            {({ isSubmitting }) => (
-        <Form className="form" className={classes.root} noValidate autoComplete="off">
-
-                {/* <TextField id="standard-basic" label="Prenom" /> */}
-            {/* <label htmlFor="prenom" className="label"> Votre Prenom </label> <br/> */}
-            <TextField id="standard-basic" name="prenom" className="input" label="Prenom"/>
-            <ErrorMessage name="prenom" component="div" className="error"/>
-
-            {/* <label htmlFor="nom" className="label"> Votre Nom de Famille </label><br/> */}
-            <TextField type="text" name="nom" className="input" label="Nom"/>
-            <ErrorMessage name="nom" component="div" className="error"/> <br/><br/>
-
-            {/* <label htmlFor="numeroDeTelephone" className="label"> Votre Numero de Telephone </label><br/> */}
-            <TextField type="text" name="numeroDeTelephone" className="input" label="Numero de Telephone"/>
-            <ErrorMessage name="numeroDeTelephone" component="div" className="error"/> 
-
-            {/* <label htmlFor="posteActuelle" className="label"> Email </label><br/> */}
-            <TextField type="text" name="email" className="input" label="E-mail"/>
-            <ErrorMessage name="email" component="div" className="error"/> <br/><br/>
-
-            {/* <label htmlFor="formation" className="label"> Quelle formation pourrait eventuellement vous interesser? </label><br/> */}
-            <TextField type="text" name="formation" className="input" label="Choix de Formation"/>
-            <ErrorMessage name="formation" component="div" className="error"/> <br/> <br/><br/>
-
-            <input type="submit" style={{marginLeft:"23%"}} value="Register" disabled={isSubmitting} className="submitButton" /><br/>
-
-        </Form>
-        )}
-        </Formik>
+        </form>
         </div>
     </div>
     );
